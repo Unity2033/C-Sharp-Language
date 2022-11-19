4c345b1b4d991090316bcb8dc050f56bf5c4d560
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-11-19 21:14:27 +0900

    Character Control System Renewal
    - Delete unnecessary code
    
    - Implemented to attach a script to
      the camera object and control only
      the position of the X axis
    
    - Implemented to move the
      character based on what the
      character is looking at
    
    - Remove walking animation

--- Assets/Script/Inheritance Object/Character.cs
+++ Assets/Script/Inheritance Object/Character.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class Character : Biology
 {
-    [SerializeField] float axisSpeed = 5.0f; // 카메라 x축과 y축의 회전 속도 
-
-    [SerializeField] GameObject eye;
-
-    private float eulerAngleX;
-    private float eulerAngleY;
+    private float mouseX;
     private int magazine = 10;
 
-    [SerializeField] float distance = 100.0f;
     [SerializeField] float speed;
+    [SerializeField] float mouseSpeed;
     [SerializeField] float gravity = 20.0f;
+    [SerializeField] float distance = 100.0f;
+
     [SerializeField] LayerMask layer;
     [SerializeField] GameObject hitEffect;
 
@@ -26,37 +23,36 @@ public class Character : Biology
 
     void Update()
     {    
-        UpdateRotate(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-
-        if (health <= 0)
-        {
-            Death();
-        }
-
         if (Input.GetButtonDown("Fire1") && magazine-- > 0)
         {      
             TwoStepRay();
             audioSource.Play();
-            animator.SetBool("Run", false);
-         
+
             if (magazine <= 0)
             {
                 StartCoroutine(Reload());
             }
         }
 
-        MoveTo(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+        direction.y -= gravity * Time.deltaTime;
 
-        // 바닥과 충돌하지 않았다면
-        if(characterControl.isGrounded == false)
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        direction = characterControl.transform.TransformDirection(direction);
+
+        characterControl.Move(direction * speed * Time.deltaTime);
+
+        mouseX += Input.GetAxis("Mouse X") * speed;
+        transform.eulerAngles = new Vector3(0, mouseX, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            // 중력을 작용하도록 설정합니다.
-            direction.y -= gravity * Time.deltaTime;
+            // 바닥과 충돌한 상태라면
+            if (characterControl.isGrounded)
+            {
+                // 점프를 할 수 있도록 설정합니다.
+                direction.y = 7.5f;
+            }
         }
-
-        characterControl.Move(direction * Time.deltaTime);
-
-        Jump();
     }
 
     private IEnumerator Reload()
@@ -70,66 +66,6 @@ public class Character : Biology
         yield return new WaitForSeconds(curAnimationTime);
 
         magazine = 10;
-    }
-
-    public void Death()
-    {
-        Time.timeScale = 0;
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;       
-    }
-
-    public void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // 바닥과 충돌한 상태라면
-            if(characterControl.isGrounded)
-            {
-                // 점프를 할 수 있도록 설정합니다.
-                direction.y = 7.5f;
-            }         
-        }
-    }
-
-    public void MoveTo(Vector3 directionForce)
-    {
-        if(characterControl.velocity != Vector3.zero && !Input.GetButtonDown("Fire1"))
-        {
-            animator.SetBool("Run", true);
-        }
-        else if(characterControl.velocity == Vector3.zero)
-        {
-            animator.SetBool("Run", false);
-        }
-
-        // 카메라 회전으로 전방 방향이 변하기 때문에 회전 값을 곱해서 연산합니다.
-        directionForce = transform.rotation * new Vector3(directionForce.x, 0, directionForce.z);
-
-        // 위/ 아래를 바라보고 이동할 때 캐릭터 오브젝트가 공중으로 이동하거나 아래로 가라앉기 때문에
-        // direction을 그대로 사용하지 않고, moveForce 변수에 x와 z값만 넣어서 사용합니다.
-        direction = new Vector3(directionForce.x * speed, direction.y, directionForce.z * speed);
-    }
-
-
-    public void UpdateRotate(float mouseX, float mouseY)
-    {
-        eulerAngleY += mouseX * axisSpeed; // 마우스 좌/우 이동으로 카메라 y축 회전
-
-        // 마우스 아래로 내리면 -로 음수인데 오브젝트의 x축이 + 방향으로 회전해야 아래를 보기 때문입니다.
-        eulerAngleX -= mouseY * axisSpeed; // 마우스 위/아래 이동으로 카메라 x축 회전
-
-        // 카메라 x축 회전의 경우 회전 범위를 설정합니다.
-        eulerAngleX = ClampAngle(eulerAngleX, -80, 50);
-
-        transform.rotation = Quaternion.Euler(transform.rotation.x, eulerAngleY, 0);
-
-        eye.transform.rotation = Quaternion.Euler(eulerAngleX, transform.eulerAngles.y, 0);
-    }
-
-    public float ClampAngle(float angle, float min, float max)
-    {
-        return Mathf.Clamp(angle, min, max);
     }
 
     public void TwoStepRay()
