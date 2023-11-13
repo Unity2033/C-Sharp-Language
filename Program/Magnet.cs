714635b3256d4457a135bad787a0f58706620903
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2023-11-13 19:47:41 +0900

    (2023.11.13) 인터페이스와 추상 클래스

---
+++ Program/Magnet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Magnet : IItem, IWaitTime
+    {
+        public void Use()
+        {
+            Console.WriteLine("Use Magnet");
+        }
+
+        public void Wait(float timer)
+        {
+            Console.WriteLine("wait Time : " + timer);
+
+        }
+    }
+}
---
+++ Program/Shield.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Shield : IItem, IWaitTime
+    {
+        public void Use()
+        {
+            Console.WriteLine("Use Shield");
+        }
+
+        public void Wait(float timer)
+        {
+            Console.WriteLine("wait Time : " + timer);
+        }
+    }
+}
