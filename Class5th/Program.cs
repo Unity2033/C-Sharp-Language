5416ab14b64f74e715d6c69b985ded7f9836aae3
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2023-11-15 19:46:29 +0900

    (2023.11.15) SOLID 원칙 1

---
+++ Class5th/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Class5th
+{
+    public delegate void Calculator(int x, int y);
+
+    internal class Program
+    {
+        static void Add(int x, int y)
+        {
+            Console.WriteLine("x + y = " + (x + y));
+        }
+
+        static void Substract(int x, int y)
+        {
+            Console.WriteLine("x - y = " + (x - y));
+        }
+
+        static void Multiply(int x, int y)
+        {
+            Console.WriteLine("x * y = " + (x * y));
+        }
+
+        static void Divide(int x, int y)
+        {
+            Console.WriteLine("x / y = " + (x / y));
+        }
+
+        static void Main(string[] args)
+        {
+            #region 델리게이트
+            // Calculator calculator;
+
+            //calculator = Add;
+
+            //calculator(10, 20);
+
+            //calculator = Substract;
+
+            //calculator(10, 20);
+            #endregion
+
+            #region 델리게이트 체인
+            //calculator = Add;
+            //calculator += Substract;
+            //calculator += Multiply;
+            //calculator += Divide;
+
+            //calculator -= Divide;
+
+            //calculator(5, 5);
+            #endregion
+
+            #region base 키워드
+            Wizard wizard = new Wizard();
+            #endregion
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -1,60 +1,28 @@
 ﻿namespace Program
 {
-    public delegate void Calculator(int x, int y);
-
     internal class Program
     {
-        static void Add(int x, int y)
-        {
-            Console.WriteLine("x + y = " + (x + y));
-        }
-
-        static void Substract(int x, int y)
-        {
-            Console.WriteLine("x - y = " + (x - y));
-        }
-
-        static void Multiply(int x, int y)
-        {
-            Console.WriteLine("x * y = " + (x * y));
-        }
-
-        static void Divide(int x, int y)
-        {
-            Console.WriteLine("x / y = " + (x / y));
-        }
-
         static void Main(string[] args)
         {
-            #region 델리게이트
-            // Calculator calculator;
-
-            //calculator = Add;
-
-            //calculator(10, 20);
-
-            //calculator = Substract;
-
-            //calculator(10, 20);
+            #region SRP 5대 원칙
+            //Monster spider = new Monster("독거미", 10, 100);
+            //spider.Patrol();
+            //Information infomation = new Information();
+            //infomation.MonsterInfo(spider);
             #endregion
 
-            #region 델리게이트 체인
-            //calculator = Add;
-            //calculator += Substract;
-            //calculator += Multiply;
-            //calculator += Divide;
+            #region OCP 5대 원칙
+            Marine marine = new Marine();
+            Firebet firebet = new Firebet();
+            Ghost ghost = new Ghost();
 
-            //calculator -= Divide;
+            UnitManager unitManager = new UnitManager();
 
-            //calculator(5, 5);
-            #endregion
-
-            #region base 키워드
-            Wizard wizard = new Wizard();
-
+            unitManager.Commend(marine);
+            unitManager.Commend(firebet);
+            unitManager.Commend(ghost);
 
             #endregion
-
         }
     }
 }
