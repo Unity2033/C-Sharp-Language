f0c628084ee6caaf19fdb23fb1e39b6b30d3ec4b
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2023-11-21 19:45:52 +0900

    (2023.11.21) 예외 처리

---
+++ Class8th/Program.cs
@@ -0,0 +1,195 @@
+﻿namespace Class8th
+{
+    public class GameObject
+    {
+        private string name;
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 콜렉션
+
+            // ArrayList
+            /*
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add(10);
+            arrayList.Add("String");
+            arrayList.Add(57.5f);
+            arrayList.Add('A');
+
+            foreach(object element in arrayList)
+            {
+                Console.WriteLine(element);
+            }
+
+            //  0       1         2      3
+            // [10] ["String"] [57.5f] ['A'] 
+
+            arrayList.Remove(10);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("arrayList Count : " + arrayList.Count);
+            Console.WriteLine(" ");
+
+            foreach (object element in arrayList)
+            {
+                Console.WriteLine(element);
+            }
+            */
+
+            // List
+            /*
+            List<int> list = new List<int>();
+
+            list.Capacity = 10;
+
+            // [10] [20] [30] [40] [50] [] [] [] [] []
+            list.Add(35);
+            list.Add(77);
+            list.Add(1);
+            list.Add(39);
+            list.Add(15);
+
+            list.Remove(45);
+            list.RemoveAt(4);
+
+            // list.Reverse(); // 리스트 안에 있는 원소를 반전하는 함수
+            // list.Sort();    // 리스트 안에 있는 원소를 정렬하는 함수    
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+          
+            List<string> games = new List<string>();
+
+            games.Capacity = 5;
+
+            games.Add("마구마구");
+            games.Add("피파온라인 4");
+            games.Add("메이플스토리");
+            games.Add("서든어택");
+            games.Add("바람의나라");
+
+            Random random = new Random();
+
+            while(games.Count != 0)
+            {
+                int rand = random.Next(0, games.Count);
+
+                Console.WriteLine(games[rand]);
+
+                games.RemoveAt(rand);
+            }
+            */
+
+            // LinkedList
+            /*
+            LinkedList<int> linkedList = new LinkedList<int>();
+
+            linkedList.AddFirst(10);
+            linkedList.AddFirst(20);
+            linkedList.AddLast(100);
+
+            foreach (var item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+            */
+
+            // Stack
+            /*
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            stack.Push(40);
+            stack.Push(50);
+
+            stack.Pop();                                       // Pop : 가장 위에 있는 데이터를 삭제합니다.
+            Console.WriteLine("stack Peek : " + stack.Peek()); // Peek : 가장 위에 있는 데이터를 반환합니다.
+
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+            */
+
+            // Queue
+            /*
+            Queue<GameObject> queue = new Queue<GameObject>();
+
+            string[] objectName = new string [4] { "Cube", "Sphere", "Cylinder", "Capsule"};
+
+            queue.Enqueue(new GameObject()); // [] 
+            queue.Enqueue(new GameObject()); // [] []
+            queue.Enqueue(new GameObject()); // [] [] []
+            queue.Enqueue(new GameObject()); // [] [] [] []
+
+            int queueSize = queue.Count;
+
+            for(int i = 0; i < queueSize; i++)
+            {
+                GameObject gameObject = queue.Dequeue();
+
+                gameObject.Name = objectName[i];
+
+                Console.WriteLine(gameObject.Name);
+            }
+            */
+
+            // HashTable
+            /*
+            Hashtable hashTable = new Hashtable();
+
+            // KEY - VALUE
+            hashTable.Add("Sword", 1000);
+            hashTable.Add('A', 57.5f);
+
+            foreach(DictionaryEntry pair in hashTable)
+            {
+                Console.WriteLine("Key : " + pair.Key + " " + "Value : " + pair.Value);
+            }
+            */
+
+            // Dictionary
+            /*
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            dictionary.Add("MapleStory", 2003);
+            dictionary.Add("League of Legend", 2011);
+            dictionary.Add("Mini Fighter", 2006);
+
+            if(dictionary.ContainsKey("MapleStory"))
+            {
+                Console.WriteLine("Key가 존재합니다.");
+            }
+
+            foreach(var element in dictionary)
+            {
+                Console.WriteLine(element);
+            }
+            */
+
+            // Random 클래스
+            /*
+            Random random = new Random();
+
+            int rand = random.Next(0, 10);
+
+            Console.WriteLine("rand : " + rand);
+            */
+            #endregion
+        }
+    }
+}
---
+++ Class9th/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+
+namespace Class9th
+{
+    public class Rifle : IEnumerator
+    {
+        private int index;
+        private int[] bullet;
+
+        public Rifle()
+        {
+            index = -1;
+            bullet = new int[5];
+        }
+
+        // Current : 읽기 전용 프로퍼티로 현재 위치의 데이터를 object 타입으로 반환합니다.
+        public object Current
+        {
+            get { return bullet[index]; }
+        }
+
+        // MoveNext() : 다음 위치로 이동하는데 다음 위치에 데이터가 있다면 true,
+        //              없다면 false를 반환하는 함수입니다.
+        public bool MoveNext()
+        {
+            index++;
+
+            if (index < bullet.Length)
+            {
+                return true;
+            }
+            else
+            {
+                Reset();
+                return false;
+            }
+        }
+
+        // Reset() : 인덱스를 초기 상태로 이동시킵니다.
+        // 컬렉션의 인덱스를 -1로 설정합니다.
+        public void Reset()
+        {
+            index = -1;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 예외 처리
+            //try
+            //{
+            //    int [] array = new int[3];
+
+            //    array[3] = 555;  
+
+            //    Console.WriteLine("프로그램 시작");
+            //}
+            //catch(Exception error)
+            //{
+            //    Console.WriteLine(error.Message);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("프로그램 종료");
+            //}
+            #endregion
+
+            #region 열거자
+
+            //Rifle rifle = new Rifle();
+
+            //while(rifle.MoveNext())
+            //{
+            //    Console.WriteLine(rifle.Current);
+            //}
+
+            #endregion
+        }
+    }
+}
