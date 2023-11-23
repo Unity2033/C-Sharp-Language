bc2473c56b66a32892d77c1b8dddd1e1feca0212
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2023-11-23 19:47:31 +0900

    (2023.11.23) 퀵 정렬

---
+++ Class11th/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Class11th
+{
+    internal class Program
+    {
+        static void BinarySearch(int[] array, int target)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                // 1. 배열의 가운데 요소의 인덱스를 pivot으로 설정합니다.
+                int pivot = (left + right) / 2;
+
+                // 2. [pivot]의 값이 찾고자 하는 요소가 같다면 검색완료
+                if (array[pivot] == target)
+                {
+                    Console.WriteLine("array[pivot] : " + array[pivot]);
+                    return;
+                } // 3. [pivot의 값이 찾는 값보다 크다면 left ~ pivot 사이를 검색합니다.]
+                else if (array[pivot] > target)
+                {
+                    right = pivot - 1;
+                }
+                else // // 4. [pivot의 값이 찾는 값보다 작다면 pivot ~ right 사이를 검색합니다.]
+                {
+                    left = pivot + 1;
+                }
+            }
+
+            Console.WriteLine("Not value Found");
+        }
+
+        static void Main(string[] args)
+        {
+            #region 이진 탐색
+            // 오름차순으로 정렬된 리스트에서 특정한 값의 위치를 찾는 알고리즘입니다.
+
+            //int[] array = new int[8] { 6, 5, 11, 13, 27, 55, 66, 99999 };
+
+            //// Array.Sort : 배열을 오름차순으로 정렬하는 함수입니다.
+            //Array.Sort(array); // [5] [6] [10] [11] [13] [27] [55] [66]
+
+            //BinarySearch(array, 99999);
+
+            #endregion
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -1,23 +1,40 @@
-﻿using System.Diagnostics;
-
-namespace Program
+﻿namespace Program
 {
     internal class Program
     {
+        static void QuickSort(int [ ] array, int left, int right)
+        {
+            // 1. left가 right 크거나 같아졌을 때
+            if(left >= right)
+            {
+                return;
+            }
+
+        }
+
+        static void Swap(ref int x, ref int y)
+        {
+            int temporary = x;
+            x = y;
+            y = temporary;
+        }
+
         static void Main(string[] args)
         {
-            #region 이진 탐색
-            // 오름차순으로 정렬된 리스트에서 특정한 값의 위치를 찾는 알고리즘입니다.
+            #region 퀵 정렬
+            // 기준점을 획득한 다음 해당 기준점을 기준으로
+            // 배열을 나누고 한 쪽에는 기준점보다 작은 항목들이
+            // 위치하고 다른 쪽에는 기준점 보다 큰 항목들이 위치합니다.
 
-            // 1. 배열의 가운데 요소의 인덱스를 pivot으로 설정합니다.
+            // 나뉘어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여
+            // 모든 배열이 기본 배열이 될 때까지 반복하면서 정렬하는 알고리즘입니다.
 
-            // 2. [pivot]의 값이 찾고자 하는 요소가 같다면 검색완료
+            // 시간복잡도 : O(log n)
 
-            // 3. [pivot의 값이 찾는 값보다 크다면 left ~ pivot 사이를 검색합니다.]
-
-            // 4. [pivot의 값이 찾는 값보다 작다면 pivot ~ right 사이를 검색합니다.]
 
             #endregion
+
+
         }
     }
 }
