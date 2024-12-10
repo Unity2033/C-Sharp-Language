b48251fffad80fec1ead660ec4af87769e53121f
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-12-10 15:35:08 +0900

    (2024.12.10) 단일 연결 리스트

---
+++ Program/Program.cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class SingleLinkedList
+{
+private:
+    int size;
+
+    struct Node
+    {
+        T data;
+        Node * next;
+    };
+
+    Node * head;
+public:
+    SingleLinkedList()
+    {
+        size = 0;
+        head = nullptr;
+    }
+
+    void PushFront(T data)
+    {
+        Node * newNode = new Node;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+
+            head->data = data;
+            head->next = nullptr;
+        }
+        else
+        {
+            newNode->data = data;
+            newNode->next = head;
+
+            head = newNode;
+        }
+
+        size++;
+
+    }
+
+    void PopFront()
+    {
+        if (head == nullptr)
+        {
+            cout << "Single Linked List is Empty" << endl;
+        }
+        else
+        {
+            Node * deleteNode = head;
+
+            head = deleteNode->next;
+
+            delete deleteNode;
+
+            size--;
+        }
+
+    }
+
+    void PushBack(T data)
+    {
+        Node * newNode = new Node;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+
+            head->data = data;
+            head->next = nullptr;
+        }
+        else
+        {
+            Node * currentNode = head;
+
+            while (currentNode->next != nullptr)
+            {
+                currentNode = currentNode->next;
+            }
+
+            currentNode->next = newNode;
+
+            newNode->data = data;
+            newNode->next = nullptr;
+        }
+
+        size++;
+    }
+
+    void Show()
+    {
+        Node * currentNode = head;
+
+        while (currentNode != nullptr)
+        {
+            cout << currentNode->data << " ";
+
+            currentNode = currentNode->next;
+        }
+    }
+
+    ~SingleLinkedList()
+    {
+        while (head != nullptr)
+        {
+            PopFront();
+        }
+
+    }
+};
+
+int main()
+{
+    SingleLinkedList<int> singleLinkedList;
+
+    singleLinkedList.PushFront(10);
+    singleLinkedList.PushFront(5);
+    singleLinkedList.PushFront(1);
+
+    singleLinkedList.PushBack(20);
+
+    singleLinkedList.PopFront();
+
+    singleLinkedList.Show();
+}
