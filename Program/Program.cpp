5f6c3b8ec32a230e64533e75ea6e682212a94e7e
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-12-17 15:45:15 +0900

    (2024.12.17) 문자열

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,9 +2,111 @@
 
 using namespace std;
 
+class String
+{
+private:
+	int size;
+	char * pointer;
+
+public:
+	String()
+	{
+		size = 0;
+		pointer = nullptr;
+	}
+
+	void operator = (const char* word)
+	{
+		size = strlen(word) + 1;
+
+		if (pointer == nullptr)
+		{
+			pointer = new char[size];
+
+			for (int i = 0; i < size; i++)
+			{
+				pointer[i] = word[i];
+			}
+		}
+		else
+		{
+			char * newPointer = new char[size];
+
+			for (int i = 0; i < size; i++)
+			{
+				newPointer[i] = word[i];
+			}
+
+			delete [ ] pointer;
+
+			pointer = newPointer;
+		}
+
+	}
+
+	void Append(const char * word)
+	{
+		size = strlen(pointer) + strlen(word) + 1;
+
+		char * newPointer = new char[size];
+
+		for (int i = 0; i < strlen(pointer); i++)
+		{
+			newPointer[i] = pointer[i];
+		}
+
+		for (int i = 0; i < strlen(word); i++)
+		{
+			newPointer[strlen(pointer) + i] = word[i];
+		}
+
+		delete [ ] pointer;
+
+		pointer = newPointer;
+
+	}
+
+	const int & Size()
+	{
+		return size - 1;
+	}
+
+	const char & operator [] (const int& index)
+	{
+		return pointer[index];
+	}
+
+	~String()
+	{
+		if (pointer != nullptr)
+		{
+			delete [ ] pointer;
+		}
+	}
+
+};
+
 int main()
 {
+	 String string;
+	 
+	 string = "Apple";
+	 
+	 for (int i = 0; i < string.Size(); i++)
+	 {
+	 	cout << string[i];
+	 }
+	 
+	 string = "Banana";
+	 
+	 string.Append("Milk");
+	 
+	 cout << endl;
+	 
+	 for (int i = 0; i < string.Size(); i++)
+	 {
+	 	cout << string[i];
+	 }
+	 return 0;
 
-
-	return 0;
 }
