87327135fdd473ec81ed43084fd30e1c863b168a
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-12-19 15:44:33 +0900

    (2024.12.19) 이진 트리

---
+++ Class9th (Hash Table)/Source.cpp
@@ -0,0 +1,208 @@
+﻿#include <iostream>
+
+#define SIZE 6
+
+using namespace std;
+
+template <typename KEY, typename VALUE>
+class HashTable
+{
+private:
+	struct Node
+	{
+		KEY key;
+		VALUE value;
+
+		Node* next;
+	};
+
+	struct Bucket
+	{
+		int count;
+		Node* head;
+	};
+
+	Bucket bucket[SIZE];
+public:
+	HashTable()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			bucket[i].count = 0;
+			bucket[i].head = nullptr;
+		}
+	}
+
+	template<typename T>
+	int HashFunction(T key)
+	{
+		unsigned int hashIndex = (int)key % SIZE;
+
+		return hashIndex;
+	}
+
+	template<>
+	int HashFunction(const char* key)
+	{
+		int result = 0;
+
+		for (int i = 0; i < strlen(key); i++)
+		{
+			result += key[i];
+		}
+
+		unsigned int hashIndex = result % SIZE;
+
+		return hashIndex;
+	}
+
+	void Insert(KEY key, VALUE value)
+	{
+		// 해시 함수를 통해서 값을 받는 임시 변수
+		int hashIndex = HashFunction(key);
+
+		// 새로운 노드를 생성합니다.
+		Node* newNode = CreateNode(key, value);
+
+		// 노드가 1개라도 존재하지 않는다면
+		if (bucket[hashIndex].count == 0)
+		{
+			// bucket[hashIndex]의 head 포인터가 newNode를 가리키게 합니다.
+			bucket[hashIndex].head = newNode;
+		}
+		else
+		{
+			newNode->next = bucket[hashIndex].head;
+
+			bucket[hashIndex].head = newNode;
+		}
+
+		// bucket[hashIndex]의 count를 증가시킵니다.
+		bucket[hashIndex].count++;
+	}
+
+	void Remove(KEY key)
+	{
+		// 1. 해시 함수를 통해서 값을 받는 임시 변수
+		int hashIndex = HashFunction(key);
+
+		// 2. Node를 탐색할 수 있는 포인터 변수 선언
+		Node* currentNode = bucket[hashIndex].head;
+
+		// 3. 이전 Node를 저장할 수 있는 포인터 변수 선언
+		Node* previousNode = nullptr;
+
+		// 4. currentNode가 nullptr과 같다면 함수를 종료합니다.
+		if (currentNode == nullptr)
+		{
+			cout << "Not Key Found" << endl;
+
+			return;
+		}
+		else
+		{
+			// 5. currentNode를 이용해서 내가 찾고자 하는 Key 값을 찾습니다.
+			while (currentNode != nullptr)
+			{
+				if (currentNode->key == key)
+				{
+					if (currentNode == bucket[hashIndex].head)
+					{
+						bucket[hashIndex].head = currentNode->next;
+					}
+					else
+					{
+						previousNode->next = currentNode->next;
+					}
+
+					bucket[hashIndex].count--;
+
+					delete currentNode;
+
+					return;
+				}
+				else
+				{
+					previousNode = currentNode;
+					currentNode = currentNode->next;
+				}
+			}
+
+			cout << "Not Key Found" << endl;
+		}
+
+
+	}
+
+	void Show()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			Node* currentNode = bucket[i].head;
+
+			while (currentNode != nullptr)
+			{
+				cout << "[" << i << "]" << " " << "{KEY : " << currentNode->key << " VALUE : " << currentNode->value << "} ";
+
+				currentNode = currentNode->next;
+			}
+
+			cout << endl;
+
+		}
+	}
+
+	Node* CreateNode(KEY key, VALUE value)
+	{
+		Node* newNode = new Node();
+
+		newNode->key = key;
+
+		newNode->value = value;
+
+		newNode->next = nullptr;
+
+		return newNode;
+	}
+
+	~HashTable()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			Node* deleteNode = bucket[i].head;
+			Node* nextNode = bucket[i].head;
+
+			if (bucket[i].head == nullptr)
+			{
+				continue;
+			}
+			else
+			{
+				while (nextNode != nullptr)
+				{
+					nextNode = deleteNode->next;
+
+					delete deleteNode;
+
+					deleteNode = nextNode;
+				}
+			}
+		}
+	}
+};
+
+int main()
+{
+	HashTable<const char*, int> hashTable;
+
+	hashTable.Insert("Sword", 10000);
+	hashTable.Insert("Armor", 25000);
+	hashTable.Insert("Posion", 5000);
+
+	hashTable.Remove("Sword");
+	hashTable.Remove("Gloves");
+
+	hashTable.Show();
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,109 +1,49 @@
 ﻿#include <iostream>
 
-#define SIZE 6
-
 using namespace std;
 
-template <typename KEY, typename VALUE>
-class HashTable
+struct Node
 {
-private:   
-	struct Node
-	{
-		KEY key;
-		VALUE value;
-
-		Node * next;
-	};
-
-	struct Bucket
-	{
-		int count;
-		Node* head;
-	};
-
-	Bucket bucket[SIZE];
-public:
-	HashTable()
-	{
-		for (int i = 0; i < SIZE; i++)
-		{
-			bucket[i].count = 0;
-			bucket[i].head = nullptr;
-		}
-	}
-
-	template<typename T>
-	int HashFunction(T key)
-	{
-		unsigned int hashIndex = (int)key % SIZE;
-
-		return hashIndex;
-	}
-
-	template<>
-	int HashFunction(const char * key)
-	{
-	    int result = 0;
-
-		for (int i = 0; i < strlen(key); i++)
-		{
-			result += key[i];
-		}				  
-
-		unsigned int hashIndex = result % SIZE;
-
-		return hashIndex;
-	}
-
-	void Insert(KEY key, VALUE value)
-	{
-		// 해시 함수를 통해서 값을 받는 임시 변수
-		int hashIndex = HashFunction(key);
-
-		// 새로운 노드를 생성합니다.
-		Node * newNode = CreateNode(key, value);
-
-		// 노드가 1개라도 존재하지 않는다면
-		if (bucket[hashIndex].count == 0)
-		{
-			// bucket[hashIndex]의 head 포인터가 newNode를 가리키게 합니다.
-			bucket[hashIndex].head = newNode;
-		}
-		else
-		{
-			newNode->next = bucket[hashIndex].head;
-
-			bucket[hashIndex].head = newNode;
-		}
-
-		// bucket[hashIndex]의 count를 증가시킵니다.
-		bucket[hashIndex].count++;
-	}
-
-	Node * CreateNode(KEY key, VALUE value)
-	{
-		Node * newNode = new Node();
-
-		newNode->key = key;
-
-		newNode->value = value;
-
-		newNode->next = nullptr;
-
-		return newNode;
-	}
+	int data;
+	Node * left;
+	Node * right;
 };
+
+Node * CreateNode(int data, Node * left, Node * right)
+{
+	Node * newNode = new Node();
+
+	newNode->data = data;
+
+	newNode->left = left;
+
+	newNode->right = right;
+
+	return newNode;
+
+}
+
+void Postorder(Node * root)
+{
+	if (root != nullptr)
+	{
+		Postorder(root->left);
+		Postorder(root->right);
+		cout << root->data << " ";
+	}
+}
 
 int main()
 {
-	HashTable<const char *, int> hashTable;
+	Node* node7 = CreateNode(7, nullptr, nullptr);
+	Node* node6 = CreateNode(6, nullptr, nullptr);
+	Node* node5 = CreateNode(5, nullptr, nullptr);
+	Node* node4 = CreateNode(4, nullptr, nullptr);
+	Node* node3 = CreateNode(3, node6, node7);
+	Node* node2 = CreateNode(2, node4, node5);
+	Node* node1 = CreateNode(1, node2, node3);
 
-	cout << hashTable.HashFunction("AWQfrwwfwf") << endl;
+	Postorder(node1);
 
-	hashTable.Insert("Sword", 10000);
-	hashTable.Insert("Armor", 25000);
-	hashTable.Insert("Posion", 5000);
-
-	 return 0;
+	return 0;
 }
