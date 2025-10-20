5b5605d980658cd1462ca6d53a955d30c033e49d
Unity2033 <kileab@naver.com>
2025-10-20 19:44:01 +0900

    (2025.10.20) 벡터

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -10,72 +10,16 @@ private:
     {
         T data;
         Node * next;
-        Node * previous;
     };
 
     int size;
     Node * head;
-    Node * tail;
 
 public:
     List()
     {
         size = 0;
         head = nullptr;
-        tail = nullptr;
-    }
-
-    void push_front(T data)
-    {
-        Node * newNode = new Node;
-
-        newNode->data = data;
-        newNode->next = nullptr;
-        newNode->previous = nullptr;
-
-        if (head == nullptr)
-        {
-            head = newNode;
-            tail = newNode;
-        }
-        else
-        {
-            head->previous = newNode;
-
-            newNode->next = head;
-
-            head = newNode;
-        }
-
-        size++;
-    }
-
-    void pop_front()
-    {
-        if (head == nullptr)
-        {
-            cout << "linked list is empty" << endl;
-        }
-        else
-        {
-            Node * deleteNode = head;
-
-            if (head == tail)
-            {
-                head = nullptr;
-                tail = nullptr;
-            }
-            else
-            {
-                deleteNode->next->previous = nullptr;
-
-                head = head->next;
-            }
-
-            delete deleteNode;
-
-            size--;
-        }
     }
 
     void push_back(T data)
@@ -83,21 +27,42 @@ public:
         Node * newNode = new Node;
 
         newNode->data = data;
-        newNode->next = nullptr;
-        newNode->previous = nullptr;
 
-        if (tail == nullptr)
+        if (head == nullptr)
         {
             head = newNode;
-            tail = newNode;
+
+            newNode->next = head;
         }
         else
         {
-            tail->next = newNode;
+            newNode->next = head->next;
 
-            newNode->previous = tail;
+            head->next = newNode;
 
-            tail = newNode;
+            head = newNode;       
+        }
+
+        size++;
+    }
+
+    void push_front(T data)
+    {
+        Node * newNode = new Node;
+
+        newNode->data = data;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+
+            newNode->next = head;
+        }
+        else
+        {
+            newNode->next = head->next;
+
+            head->next = newNode;
         }
 
         size++;
@@ -110,14 +75,11 @@ int main()
 {
     List<int> list;
 
-    list.push_front(10);
-    list.push_front(5);
-
+    list.push_back(10);
     list.push_back(20);
-    list.push_back(30);
 
-    list.pop_front();
-    list.pop_front();
+    list.push_front(5);
+    list.push_front(1);
 
     return 0;
 }
