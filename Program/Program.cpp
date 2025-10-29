226021e871179ca404b7d948fe678a5af63ed5da
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-10-29 19:46:48 +0900

    (2025.10.29) 인접 행렬

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,8 +2,76 @@
 
 using namespace std;
 
+template<typename KEY, typename VALUE>
+class HashTable
+{
+private:
+    struct Node
+    {
+        KEY key;
+        VALUE value;
+
+        Node * next;
+    };
+
+    struct Bucket
+    {
+        int count;
+        Node * head;
+    };
+
+    int size;
+    int capacity;
+
+    Bucket * bucket;
+
+public:
+    HashTable()
+    {
+        size = 0;
+        capacity = 8;
+
+        bucket = new Bucket[capacity];
+
+        for (int i = 0; i < capacity; i++)
+        {
+            bucket[i].head = nullptr;
+            bucket[i].count = 0;
+        }
+    }
+
+    template<typename KEY>
+    unsigned int hash_function(KEY key)
+    {
+        return (unsigned int)key % capacity;
+    }
+
+    template<>
+    unsigned int hash_function(const char * key)
+    {
+        int sum = 0;
+
+        for (int i = 0; *key != '\0'; i++)
+        {
+            sum += key[i];
+
+            key = key + 1;
+        }
+
+        return (unsigned int)sum % capacity;
+    }
+};
+
 int main()
 {
+    HashTable<const char*, int> hashtable;
+
+    cout << hashtable.hash_function("Ko115frea") << endl;
+    cout << hashtable.hash_function("rqwr") << endl;
+    cout << hashtable.hash_function("Chirteyna") << endl;
+    cout << hashtable.hash_function("C5qdqdyna") << endl;
+    cout << hashtable.hash_function("C6wrrwrwyna") << endl;
+    cout << hashtable.hash_function("Chi868na") << endl;
 
     return 0;
 }
